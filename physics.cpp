e5b319217682a6668c7d991123affda99001e117
Jenna Ray <ray19008@byui.edu>
2025-02-22 12:37:20 -0700

    added physics file

---
+++ physics.cpp
@@ -0,0 +1,8 @@
+/*****************************************
+* All the physics we need for the prototype
+******************************************/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
