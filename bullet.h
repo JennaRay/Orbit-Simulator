bcc00766e9b5aae5e66fd7b2cd73c15055f9cbb2
Jenna Ray <ray19008@byui.edu>
2025-04-05 10:32:51 -0600

    bullets expire after 70 frames. Bullet code moved to cpp file.

--- bullet.h
+++ bullet.h
@@ -13,37 +13,27 @@ public:
 		// Set the position and velocity
 		pos.addPixelsX(20 * sin(parent.getSpin()));
 		pos.addPixelsY(20 * cos(parent.getSpin()));
-		v.addDX(((thrust/4) + 9000) * sin(parent.getSpin()));
-		v.addDY(((thrust/4) + 9000) * cos(parent.getSpin()));
+		v.addDX(((thrust/10) + 9000) * sin(parent.getSpin()));
+		v.addDY(((thrust/10) + 9000) * cos(parent.getSpin()));
 
 		setPosition(pos);
 		setVelocity(v);
 		Angle newAngle = Angle(parent.getSpin());
 		setAngle(newAngle);
-		setRadius(2); // set the bullet radius to 2 pixels so that it can collide (like spaceplane)
-		expireTime = 100;
+		setRadius(1); // set the bullet radius to 2 pixels so that it can collide (like spaceplane)
+		expireTime = 70;
+		expireCount = 0;
       setCollide(false);
 	}
 	void collide() override
 	{
 		setCollide(true);
 	}
-	void moveForward(double time) {
+	void moveForward(double time);
 
-		Position pos = getPosition();
-		double newX = pos.getMetersX() + getVelocity().getDX() * time + 0.5 * 1 * pow(time, 2);
-		double newY = pos.getMetersY() + getVelocity().getDY() * time + 0.5 * 1 * pow(time, 2);
-
-		setPosition(newX, newY);
-
-		//expireTime--;
-		/*if (expireTime <= 0)
-			setCollide(true);*/
-
-	}
-
-	void draw(ogstream& gout) override { gout.drawProjectile(getPosition()); }
+	void draw(ogstream& gout) override;
 
 private: 
 	int expireTime;
+	int expireCount;
 };
